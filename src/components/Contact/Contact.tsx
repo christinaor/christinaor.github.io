56f080916995c5eb79bd583ce1cb7f3aabf8a150
Christina Or <christinaor.tech@gmail.com>
2023-02-28 00:57:22 -0500

    Update styling

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -9,34 +9,31 @@ function Contact() {
     <section id="contact-me" className={`${styles.contactMe}`}>
       <h3 className={`${styles.header}`}>Get in Touch</h3>
       <div>
-        <p>I'm currently exploring new opportunities. Feel free to reach out to collaborate, chat about tech, or if you just want to say hi!</p>
+        <p className={`${styles.contactBlurb}`}>I'm currently exploring new opportunities. Feel free to reach out to collaborate, chat about tech, or if you just want to say hi!</p>
 
-        <div className={`${styles.contactInfo}`}>
-          <div className={`${styles.socials}`}>
-            {/* <p>Find me on social media:</p> */}
-            <ul>
-              <li>
-                <a className="linkedin" href="https://www.linkedin.com/in/christina-or">
-                  <img src={linkedinIcon} className="linkedin-logo" alt="LinkedIn logo" />
-                </a>
-              </li>
-              <li>
-                <a className="github" href="https://www.github.com/christinaor">
-                  <img src={githubIcon} className="github-logo" alt="GitHub logo" />
-                </a>
-              </li>
-              <li>
-                <a className="instagram" href="https://www.instagram.com/christinahahaaa">
-                  <img src={instagramIcon} className="instagram-logo" alt="Instagram logo" />
-                </a>
-              </li>
-              <li>
-                <a id="contact" className="gmail" href={"mailto:".concat(atob("b3IuY2hyaXN0aW5hMjdAZ21haWwuY29t"))} rel="noopener noreferrer">
-                  <img src={gmailIcon} className="gmail-logo" alt="Gmail logo" />
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className={`${styles.contactLinks}`}>
+          <ul>
+            <li>
+              <a className="linkedin" href="https://www.linkedin.com/in/christina-or">
+                <img src={linkedinIcon} className="linkedin-logo" alt="LinkedIn logo" />
+              </a>
+            </li>
+            <li>
+              <a className="github" href="https://www.github.com/christinaor">
+                <img src={githubIcon} className="github-logo" alt="GitHub logo" />
+              </a>
+            </li>
+            <li>
+              <a className="instagram" href="https://www.instagram.com/christinahahaaa">
+                <img src={instagramIcon} className="instagram-logo" alt="Instagram logo" />
+              </a>
+            </li>
+            <li>
+              <a id="contact" className="gmail" href={"mailto:".concat(atob("b3IuY2hyaXN0aW5hMjdAZ21haWwuY29t"))} rel="noopener noreferrer">
+                <img src={gmailIcon} className="gmail-logo" alt="Gmail logo" />
+              </a>
+            </li>
+          </ul>
         </div>
 
       </div>
