46309284df145f2a40b1f55caa5d3bb4b534c7cd
Christina Or <christinaor.tech@gmail.com>
2023-03-18 23:42:56 -0400

    Update hero font size to rem

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import styles from './styles.module.scss'
 
 function Hero() {
@@ -9,17 +8,8 @@ function Hero() {
         <div className={styles.bigHeroText}>Christina Or.</div>
         <div className={`${styles.subText}`}>Software Engineer && Web Developer.</div>
       </h1>
-      {/* <h1 className={`${styles.bigHeroText}`}></h1>
-      <h1 className={`${styles.subText}`}></h1> */}
 
-      <p>I love building out ideas that save precious time and contributing to efforts promoting general goodwill.</p>
-      {/* <div className={`${styles.sameLineText}`}>
-        <h2 className={`${styles.bigHeroText}`}>Christina Or.</h2>
-        <h1 className={`${styles.smallHeroText}`}>and I'm a</h1>
-      </div>
-      <h2 className={`${styles.subText}`}>I am a Software Engineer && Web Developer.</h2> */}
-      {/* <h2 className={`${styles.subText}`}>Software Engineer</h2> */}
-      {/* <h2 className={`${styles.subText}`}>&& Web Developer.</h2> */}
+      <p className={styles.heroBlurb}>I love building out ideas that save precious time and contributing to efforts promoting general goodwill.</p>
     </section>
   )
 }
