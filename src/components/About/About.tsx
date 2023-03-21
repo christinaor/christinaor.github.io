23a265a787bd876b2457975a9539227eb00c5c7b
Christina Or <christinaor.tech@gmail.com>
2023-03-20 20:24:39 -0400

    Update padding and margin to rem

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,6 @@ import styles from './styles.module.scss';
 function About() {
   return (
     <section id="about" className={styles.about}>
-      {/* <div className={styles.blurb}> */}
         <h2 className={`${styles.header}`}>About Me</h2>
         <div className={styles.description}>
           <p> In more recent years, I've found a love for automation and building full-scale web applications.</p>
@@ -14,7 +13,6 @@ function About() {
             <li>I'm currently volunteering for a civic-tech organization called <a className={styles.blurbLink} href="https://www.hackforla.org/" target="_blank" rel="noopener noreferrer">Hack for LA</a>, where I help build products with other developers to alleviate issues in the LA region.</li>
           </ul>  
         </div>
-      {/* </div> */}
       <TechStack />
     </section>
   )
