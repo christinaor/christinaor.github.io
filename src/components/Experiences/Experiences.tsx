729ec0e04061a06903f28769d5916ae0f50fc44f
Christina Or <christinaor.tech@gmail.com>
2023-03-22 01:55:24 -0400

    Initial experience add

--- src/components/Experiences/Experiences.tsx
+++ src/components/Experiences/Experiences.tsx
@@ -1,12 +1,33 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+
+import reactimeGif from '../../assets/experience-images/reactime.gif'
+
+import githubLinkLogo from '../../assets/experience-images/github-svgrepo-com.svg'
+import websiteIcon from '../../assets/experience-images/website-dashboard-svgrepo-com.svg'
+
 import styles from './styles.module.scss'
 
-function Experiences() {
+export default function Experiences() {
   return (
-    <section id="experiences" className={`${styles.experiences}`}>
-      <h3 className={`${styles.header}`}>Some Of My Work ~ Coming Soon</h3>
+    <section id="experiences" className={`${styles.experienceSection}`}>
+      <h2 className={`${styles.header}`}>Experiences</h2>
+      <div className={styles.experiences}>
+        <div className={styles.experience}>
+          <a href="https://medium.com/@christinaor/uncovering-reactime-understanding-react-fiber-redux-compatibility-and-zoning-in-on-components-59f0758e0fa3"><h3>Reactime</h3></a>
+          <img src={reactimeGif} alt="picture of reactime" />
+          <p>Built features for a React performance tool that monitors application state snapshots and allows developers to switch between snapshots.</p>
+          <div className={styles.links} >
+            <a href="https://github.com/open-source-labs/reactime">
+              <img src={githubLinkLogo} alt="GitHub logo linking to Reactime repository" />
+            </a>
+            <a href="https://reactime.dev">
+              <img src={websiteIcon} alt="website icon linking to Reactime website" />
+            </a>
+          </div>
+        </div>
+      </div>
+      
+
     </section>
   )
-}
-
-export default Experiences;
+};
