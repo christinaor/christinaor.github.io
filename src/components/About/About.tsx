39f663aa859c95200a1e7f62b55c0a5211c216c2
Christina Or <christinaor.tech@gmail.com>
2023-03-24 22:20:29 -0400

    Add about item

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -11,6 +11,7 @@ export default function About() {
             <li>At <a className={styles.blurbLink} href="https://www.nielsen.com" target="_blank" rel="noopener noreferrer">Nielsen</a>, I enjoyed writing Python scripts for recurring projects and sharing my technical knowledge to help colleagues upskill and boost the automation initiative.</li>
             <li>My last role at an early stage startup called <a className={styles.blurbLink} href="https://www.skematic.com" target="_blank" rel="noopener noreferrer">Skematic</a> involved writing business logic to solve complex issues and building out features across the application in the niche world of compliance tech.</li>
             <li>I'm currently volunteering for a civic-tech organization called <a className={styles.blurbLink} href="https://www.hackforla.org/" target="_blank" rel="noopener noreferrer">Hack for LA</a>, where I help build products with other developers to alleviate issues in the LA region.</li>
+            <li>Some of my interests involve dragon boat racing, board game nights, traveling to learn about different cultures and food, and being a dog mom!</li>
           </ul>  
         </div>
       <TechStack />
