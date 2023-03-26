a7b729917068d54779dde943d32a21ed0b37fd81
Christina Or <christinaor.tech@gmail.com>
2023-03-26 04:06:54 -0400

    Change home id

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.scss'
 
 export default function Hero() {
   return (
-    <section id="hero" className={styles.hero}>
+    <section id="home" className={styles.hero}>
       <h1>
         <div className={styles.smallHeroText}>Hi there, I am</div>
         <div className={styles.bigHeroText}>Christina Or.</div>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -20,7 +20,7 @@ export default function NavBar() {
 
   return (
     <nav id="nav-bar" className={`${styles.navBar} ${isScrolled ? styles.isScrolled : ''}`}>
-      <a className={styles.home} href="/#hero">
+      <a className={styles.home} href="#home">
         <img src={homeIcon} alt="home icon linked to top of page" />
       </a>
 
