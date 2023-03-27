4fb217fbf002ef3377a1b02ab1b17296bf6a8b50
Christina Or <christinaor.tech@gmail.com>
2023-03-27 16:40:32 -0400

    Add data and fix tech section for mobile and desktop

---
+++ data/experiences.tsx
@@ -0,0 +1,30 @@
+import reactimeGif from '../src/assets/experience-images/reactime.gif'
+import portfolioImg from '../src/assets/experience-images/portfolio-site.png'
+
+const experiences = [
+  {
+    title: "Reactime",
+    articleLink: "https://medium.com/@christinaor/uncovering-reactime-understanding-react-fiber-redux-compatibility-and-zoning-in-on-components-59f0758e0fa3",
+    description: "Built features for a React performance tool that monitors application state snapshots and allows developers to switch between snapshots.",
+    image: reactimeGif,
+    imageAlt: "picture of Reactime",
+    githubLink: "https://github.com/open-source-labs/reactime",
+    githubLinkImageAlt: "GitHub logo linking to Reactime repository",
+    websiteLink: "https://reactime.dev",
+    websiteLinkImageAlt: "website icon linking to Reactime website",
+    technologies: ["TypeScript", "React", "D3.js", "Redux DevTools"]
+  },
+  {
+    title: "Personal Portfolio",
+    description: "My personal portfolio website where I share my background, passion in software engineering, and showcase some of the projects I've lent a hand in building.",
+    image: portfolioImg,
+    imageAlt: "picture of portfolio home view",
+    githubLink: "https://github.com/christinaor/christinaor.github.io",
+    githubLinkImageAlt: "GitHub logo linking to portfolio site repository",
+    websiteLink: "christinaor.github.io/",
+    websiteLinkImageAlt: "website icon linking to portfolio website",
+    technologies: ["TypeScript", "React", "Vite"]
+  }
+]
+
+export default experiences;
--- src/components/Experiences/Experiences.tsx
+++ src/components/Experiences/Experiences.tsx
@@ -1,6 +1,6 @@
-// import { useState } from 'react';
+import experiencesData from '../../../data/experiences'
 
-import reactimeGif from '../../assets/experience-images/reactime.gif'
+// import reactimeGif from '../../assets/experience-images/reactime.gif'
 
 import githubLinkLogo from '../../assets/experience-images/github-svgrepo-com.svg'
 import websiteIcon from '../../assets/experience-images/external-link-svgrepo-com.svg'
@@ -12,22 +12,65 @@ export default function Experiences() {
     <section id="experiences" className={`${styles.experienceSection}`}>
       <h2 className={`${styles.header}`}>Experiences</h2>
       <div className={styles.experiences}>
-        <div className={styles.experience}>
-          <a href="https://medium.com/@christinaor/uncovering-reactime-understanding-react-fiber-redux-compatibility-and-zoning-in-on-components-59f0758e0fa3"><h3>Reactime</h3></a>
-          <img src={reactimeGif} alt="picture of reactime" />
-          <p>Built features for a React performance tool that monitors application state snapshots and allows developers to switch between snapshots.</p>
-          <div className={styles.links} >
-            <a href="https://github.com/open-source-labs/reactime">
-              <img src={githubLinkLogo} alt="GitHub logo linking to Reactime repository" />
-            </a>
-            <a href="https://reactime.dev">
-              <img src={websiteIcon} alt="website icon linking to Reactime website" />
-            </a>
-          </div>
-        </div>
+        {experiencesData?.map(experience => {
+          return (
+            <div className={styles.experience}>
+            
+              <a href={experience?.articleLink ? experience.articleLink : ''}><h3>{experience.title}</h3></a>
+
+              <img className={styles.experienceImage} src={experience.image} alt={experience.imageAlt} />
+
+              <p>{experience.description}</p>
+
+              {/* <div className={styles.technologiesAndLinks}> */}
+                <ul className={styles.technologies}>
+                  {experience?.technologies && experience.technologies.map(technology => (
+                    <li>{technology}</li>
+                  ))}
+                </ul>
+
+                <div className={styles.links} >
+                  <a href={experience?.githubLink}>
+                    <img src={githubLinkLogo} alt={experience.githubLinkImageAlt} />
+                  </a>
+                  <a href={experience?.websiteLink}>
+                    <img src={websiteIcon} alt={experience.websiteLinkImageAlt} />
+                  </a>
+                </div>
+              {/* </div> */}
+
+            </div>
+          )
+        })}
+
       </div>
       
 
     </section>
   )
 };
+
+// export default function Experiences() {
+//   return (
+//     <section id="experiences" className={`${styles.experienceSection}`}>
+//       <h2 className={`${styles.header}`}>Experiences</h2>
+//       <div className={styles.experiences}>
+//         <div className={styles.experience}>
+//           <a href="https://medium.com/@christinaor/uncovering-reactime-understanding-react-fiber-redux-compatibility-and-zoning-in-on-components-59f0758e0fa3"><h3>Reactime</h3></a>
+//           <img src={reactimeGif} alt="picture of reactime" />
+//           <p>Built features for a React performance tool that monitors application state snapshots and allows developers to switch between snapshots.</p>
+//           <div className={styles.links} >
+//             <a href="https://github.com/open-source-labs/reactime">
+//               <img src={githubLinkLogo} alt="GitHub logo linking to Reactime repository" />
+//             </a>
+//             <a href="https://reactime.dev">
+//               <img src={websiteIcon} alt="website icon linking to Reactime website" />
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+      
+
+//     </section>
+//   )
+// };
