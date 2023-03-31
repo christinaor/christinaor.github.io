41445f423f63cb345e311f01e0c0a50a397b6e42
Christina Or <christinaor.tech@gmail.com>
2023-03-31 15:24:55 -0400

    Add experience carousel logic

--- src/components/Experiences/Experiences.tsx
+++ src/components/Experiences/Experiences.tsx
@@ -1,18 +1,75 @@
-import experiencesData from '../../../data/experiences'
+import { useState } from 'react';
 
-// import reactimeGif from '../../assets/experience-images/reactime.gif'
+import experiencesData from '../../../data/experiences';
 
-import githubLinkLogo from '../../assets/experience-images/github-svgrepo-com.svg'
-import websiteIcon from '../../assets/experience-images/external-link-svgrepo-com.svg'
+import arrowIcon from '../../assets/arrow.svg';
 
-import styles from './styles.module.scss'
+import githubLinkLogo from '../../assets/experience-images/github-svgrepo-com.svg';
+import websiteIcon from '../../assets/experience-images/external-link-svgrepo-com.svg';
+
+import styles from './styles.module.scss';
 
 export default function Experiences() {
+  const [cardIndex, setCardIndex] = useState(0);
+
+  const cardsArray = experiencesData?.map(experience => {
+    return (
+      <div className={styles.experience}>
+        <div className={styles.titleAndLinks}>
+          <a href={experience?.articleLink ? experience.articleLink : ''}><h3>{experience.title}</h3></a>
+
+          <div className={styles.links} >
+            <a href={experience?.githubLink}>
+              <img src={githubLinkLogo} alt={experience.githubLinkImageAlt} />
+            </a>
+            <a href={experience?.websiteLink}>
+              <img src={websiteIcon} alt={experience.websiteLinkImageAlt} />
+            </a>
+          </div>
+        </div>
+
+        <img className={styles.experienceImage} src={experience.image} alt={experience.imageAlt} />
+
+        <p>{experience.description}</p>
+
+        <ul className={styles.technologies}>
+          {experience?.technologies?.map(technology => (
+            <li className={styles.technology}>{technology}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  })
+
+  const handleLeftArrowClick = () => {
+    console.log('left clicked')
+    if (cardIndex - 1 >= 0) {
+      setCardIndex(cardIndex - 1)
+    } else {
+      setCardIndex(cardsArray.length - 1)
+    }
+  };
+
+  const handleRightArrowClick = () => {
+    console.log('right clicked')
+    if (cardIndex + 1 < cardsArray.length) {
+      setCardIndex(cardIndex + 1)
+    } else {
+      setCardIndex(0)
+    }
+  }
+
   return (
     <section id="experiences" className={`${styles.experienceSection}`}>
       <h2 className={`${styles.header}`}><span className='section-number'>3. </span>Experiences</h2>
       <div className={styles.experiences}>
-        {experiencesData?.map(experience => {
+        <img className={styles.leftArrow} src={arrowIcon} alt="left arrow icon for moving left in experience cards" onClick={handleLeftArrowClick}/>
+        {cardsArray && cardsArray[cardIndex]}
+        <img className={styles.rightArrow} src={arrowIcon} alt="left arrow icon for moving left in experience cards" onClick={handleRightArrowClick} />
+
+        {/* {experiencesData?.map(experience => {
+
+
           return (
             <div className={styles.experience}>
               <div className={styles.titleAndLinks}>
@@ -37,11 +94,9 @@ export default function Experiences() {
                   <li className={styles.technology}>{technology}</li>
                 ))}
               </ul>
-
-              
             </div>
           )
-        })}
+        })} */}
 
       </div>
       
@@ -49,28 +104,3 @@ export default function Experiences() {
     </section>
   )
 };
-
-// export default function Experiences() {
-//   return (
-//     <section id="experiences" className={`${styles.experienceSection}`}>
-//       <h2 className={`${styles.header}`}>Experiences</h2>
-//       <div className={styles.experiences}>
-//         <div className={styles.experience}>
-//           <a href="https://medium.com/@christinaor/uncovering-reactime-understanding-react-fiber-redux-compatibility-and-zoning-in-on-components-59f0758e0fa3"><h3>Reactime</h3></a>
-//           <img src={reactimeGif} alt="picture of reactime" />
-//           <p>Built features for a React performance tool that monitors application state snapshots and allows developers to switch between snapshots.</p>
-//           <div className={styles.links} >
-//             <a href="https://github.com/open-source-labs/reactime">
-//               <img src={githubLinkLogo} alt="GitHub logo linking to Reactime repository" />
-//             </a>
-//             <a href="https://reactime.dev">
-//               <img src={websiteIcon} alt="website icon linking to Reactime website" />
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-      
-
-//     </section>
-//   )
-// };
