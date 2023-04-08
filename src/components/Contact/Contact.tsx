08543d06a2b9441e21ad2afa4bfc149ac7d24744
Christina Or <christinaor.tech@gmail.com>
2023-04-08 03:46:44 -0400

    Fix Contact and Exp sections for mobile

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
   return (
     <section id="contact-me" className={`${styles.contactMe}`}>
       <h2 className={`${styles.header}`}>
-        {/* <span className='section-number'>4. </span> */}
+        <span className='section-number'>4. </span>
         Get in Touch</h2>
       <p className={`${styles.contactBlurb}`}>I'm currently exploring new opportunities. Reach out to collaborate, chat about tech, or just to say hi!</p>
 
