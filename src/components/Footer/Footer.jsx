b98c40905d7d1f9733b828b7df0a89e3e3d9ff72
Christina Or <christinaor.tech@gmail.com>
2023-04-12 22:38:14 -0400

    Adjust styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ import styles from './styles.module.scss';
 
 export default function Footer() {
   return (
-    <footer id="about" className={styles.footer}>
+    <footer id="footer" className={styles.footer}>
       <div className={styles.links}>
         <div>
           <h3 className={styles.footerTitle}>Reach out to me</h3>
